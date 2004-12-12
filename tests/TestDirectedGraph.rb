26e313328110ba1e2e91838fbd9b9e0f6261b5fb
John Carter <cyent@xnet.co.nz>
2004-12-12 19:08:35 +0000

    Added test_random

--- tests/TestDirectedGraph.rb
+++ tests/TestDirectedGraph.rb
@@ -97,4 +97,28 @@ class TestDirectedGraph < Test::Unit::TestCase
 	assert_equal(dg.vertices.sort, [1,2,3,4])
 	assert_equal(dg.edges.to_s, "(1-2)(3-4)")
   end
+
+  def test_random
+    complete = DirectedAdjacencyGraph.random( 10, 1)
+    assert_equal( 10, complete.size)
+    assert_equal( 10*10, complete.num_edges)
+
+    trivial = DirectedAdjacencyGraph.random( 10, 0)
+    assert_equal( 10, trivial.size)
+    assert_equal( 0, trivial.num_edges)
+
+
+    random = DirectedAdjacencyGraph.random( 1000, 0.01)
+    assert_equal( 1000, random.size)
+
+    # On average there are going to be 1000*1000*0.01 = 10000 edges.
+    # It is _very_ unlikely to 100 times more or less than that
+    assert( 1000 * 1000 * 0.01 / 100 < random.num_edges)
+    assert( 1000 * 1000 * 0.01 * 100 > random.num_edges)
+  end
 end
+
+# Tell emacs that we are using 4 space tabs..
+# Local Variables:
+# tab-width: 4
+# End:
